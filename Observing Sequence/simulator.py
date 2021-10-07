b1ce70bdd2757f3b437998ee1d4083762ebf6deb
awaismirza92 <awais@uni-bonn.de>
2021-10-07 21:54:00 +0200

    added GMVA as the array name

--- Observing Sequence/simulator.py
+++ Observing Sequence/simulator.py
@@ -8,7 +8,7 @@ import glob
 
 #Specifying the working directory
 current_dir = os.getcwd()
-print(current_dir)
+print('Current working directory: ' + current_dir + '\n')
 
 if 'small_ms' or 'per_scan_ms' in current_dir:
     file_address = '../../input_files/c171a'
@@ -16,29 +16,30 @@ if 'small_ms' or 'per_scan_ms' in current_dir:
 else:
     file_address = 'input_files/c171a'
 
+print('Directory for GMVA session files: ' + file_address + '\n')
 
-print(file_address)
 #%%
-
 #Executing the python file to extract observing sequence
 execfile('../../observing_sequence_reader.py')
 
 execfile('../../model_vlbi_functions.py')
 
-# import model_vlbi_functions
+
+# Specifying the name of the array (e.g. 'GMVA')
+# array = 'GMVA'
+array = 'VLA'
+
+
+vlbi_vp(scans, modes, station_abbrev_dic, dia_dic, blockage_diam='0.75m', max_rad='1.000deg')
+
+
 
 # %%
-
-
-
-   
-# perform_observation(scans, dia, station_names, pos_obs, modes, integration_time)
+perform_observation(scans, dia, station_names, pos_obs, modes, sources, integration_time)
 
 
 # %%
-
-# mylengths, beam_max, lambd, pix_res = compute_beam_max_and_pix_res(x_adj_dic, y_adj_dic, z_adj_dic, modes)
-
+mylengths, beam_max, lambd, pix_res = compute_beam_max_and_pix_res(x_adj_dic, y_adj_dic, z_adj_dic, modes)
 
 
 # %%
@@ -47,27 +48,21 @@ flux_file_existance_check(sources, start_date, source_fluxes_address)
 
 
 # %%
+flux_sources = flux_file_completion_check(sources, source_fluxes_address)
 
 
-        
-# flux_sources = flux_file_completion_check(sources, start_date, source_fluxes_address)
-         
-# create_input_models(sources, pix_res, modes, flux_sources)            
-        
-# #Create noisy ms.
-create_noisy_ms(scans, freq_setup, integration_time, npol)
-    
+create_input_models(sources, pix_res, modes, flux_sources)
 
-    
-#%%
+#Create noisy ms.
+create_noisy_ms(scans, freq_setup, integration_time, npol, flux_sources)
 
-# corrupt_model_data(scans)     
-    
 
 
 #%%
+corrupt_model_data(scans, pix_res)
 
-# combine_measurment_sets(scans)
+#%%
+combine_measurement_sets(scans)
 
 
     
