aeb587a6ee67ddbccff846b08f8a8deba600995d
awaismirza92 <awais@uni-bonn.de>
2022-04-17 15:33:17 +0200

    added tsys antabfs address variable

--- Observing Sequence/input_parameters.py
+++ Observing Sequence/input_parameters.py
@@ -2,10 +2,22 @@
 
 
 #specify the address of vex file
-vex_file_address = 'input_files/c171a.vex.obs'
+vex_file_address = 'input_files/n21q1.vex'
 
 #specify the address of sum file
-sum_file_address = 'input_files/c171a.sum'
+sum_file_address = 'input_files/n21q1.sum'
+
+#specify the address of directory containing stations antabfs files
+antabfs_files_address = 'input_files/'
 
 #specify the address of the file containing source fluxes
 source_fluxes_address = 'source_fluxes.txt'
+
+#specify the name of the array (e.g. 'GMVA', 'VLA', 'EVN')
+array = 'EVN'
+
+#specify the first and last scan to be simulated. To simulate all the scans, use
+# start_scan = 1 and end_scan = 'until_last_scan'
+start_scan = 1
+end_scan = 4
+
