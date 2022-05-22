1ea1249d9a0397204e3ac619e98aba3169f64cdf
awaismirza92 <awais@uni-bonn.de>
2022-05-22 22:17:37 +0200

    added address of antabsfs files

--- Observing Sequence/input_parameters.py
+++ Observing Sequence/input_parameters.py
@@ -10,14 +10,17 @@ sum_file_address = 'input_files/n21q1.sum'
 #specify the address of directory containing stations antabfs files
 antabfs_files_address = 'input_files/'
 
+#specify the address of SCHED source catalog file
+sources_gsfc2016_files_address = 'input_files/sources.gsfc2016.txt'
+
 #specify the address of the file containing source fluxes
 source_fluxes_address = 'source_fluxes.txt'
 
 #specify the name of the array (e.g. 'GMVA', 'VLA', 'EVN')
 array = 'EVN'
 
-#specify the first and last scan to be simulated. To simulate all the scans, use
+#specify the first and last scan to be simulated. To simulate all the scans, set
 # start_scan = 1 and end_scan = 'until_last_scan'
-start_scan = 1
-end_scan = 4
+start_scan = 2
+end_scan = 3
 
